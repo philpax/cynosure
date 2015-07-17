1545c533db8fb37842b5472fe5508c50125450d8
Philpax <me@philpax.me>
2015-07-18 04:55:23 +1000

    Removed CONV_8B_TO_16/32B

--- src/utils.hpp
+++ src/utils.hpp
@@ -17,12 +17,6 @@
 
 #define SIGN(value) ((value) < 0 ? -1 : ((value) > 0 ? 1 : 0))
 
-// Converts four 8-bit integers to one 32-bit integer
-#define CONV_8B_TO_32B(a, b, c, d)                                                                 \
-    ((uint32_t)(a) | ((uint32_t)(b) << 8) | ((uint32_t)(c) << 16) | ((uint32_t)(d) << 24))
-// Converts two 8-bit integers to one 16-bit integer
-#define CONV_8B_TO_16B(a, b) ((uint32_t)(a) | ((uint32_t)(b) << 8))
-
 // Provides a hexadecimal value with correct padding, etc for printing
 #define PRINT_VALUE(value)                                                                         \
     "0x" << std::hex << std::uppercase << (state->CR0.protectedMode ? std::setw(8) : std::setw(4)) \
